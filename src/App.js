622c4f7ed131be28993f293bd29a6522c0299fc3
Niki Fereidooni <nikifereidooni@gmail.com>
2022-02-02 13:33:53 -0500

    fixed routing issue and added content to test page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css"
 import "./App.css";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import TestPage from "./components/TestPage";
 import Footer from "./components/Footer";
@@ -10,9 +10,11 @@ function App() {
 
   return (
     <Router basename={process.env.PUBLIC_URL}>
-          <Header />
-          <TestPage />
-          <Footer />
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<TestPage />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/TestPage/index.js
@@ -0,0 +1,14 @@
+import { React } from "react";
+import "./style.css";
+
+
+function TestPage() {
+
+  return (
+    <div>
+        TEST 123
+    </div>
+  );
+}
+
+export default TestPage;
